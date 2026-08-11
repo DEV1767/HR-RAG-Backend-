cd7064453c2452baab5a881e90789f70e0e3cb81
shivam chaudhary <shivamchy076@gmail.com>
2026-08-11 19:05:52 +0530

    Upadted index.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,12 +2,15 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
     try {
-        const connection = await mongoose.connect(process.env.MONGO_URL)
-        console.log("Mongo DB connected")
+        console.log("MONGO_URL exists:", !!process.env.MONGO_URL);
+
+        const connection = await mongoose.connect(process.env.MONGO_URL);
+
+        console.log("Mongo DB connected:", connection.connection.host);
     } catch (error) {
-        console.log("Mongo DB  connection failed", error.message)
+        console.log("Mongo DB connection failed:", error.message);
         process.exit(1);
     }
-}
+};
 
-export default connectDb
+export default connectDb;
